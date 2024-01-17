5d42f4a9d0783c0d7bfe10ba316cc191118ded55
Kaue Piovan <kaupiovan@gmail.comm>
2024-01-17 19:56:53 -0300

    .

---
+++ secao01/aula01.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+console.log("Hello World! '1'"); // string
+console.log('Hello World! "2"'); // string
+console.log(`Hello World '"3"'`); // string
+console.log(10, 15.857); // number
+
+console.log(10, 15.8571245, "Hello World!");
